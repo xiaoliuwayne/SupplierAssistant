ac15db0b57766b0c5de049e44db84cd3d7df24ad
wayne <1005881270@qq.com>
2019-03-19 09:21:15 +0800

    修改tabbar

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
-    <tabbar @on-index-change="onIndexChange" class="tabbar-style" >
-      <tabbar-item selected link="/home">
+    <router-view></router-view>
+    <tabbar @on-index-change="onIndexChange" class="tabbar-style">
+      <tabbar-item link="/home" v-model="itemIndex">
         <img slot="icon" src="../assets/img/home.png">
         <img slot="icon-active" src="../assets/img/home-ed.png">
         <span slot="label">首页</span>
@@ -26,16 +27,41 @@
 </template>
 
 <script>
+  // import {mapGetters} from 'vuex'
     export default {
         name: "Index",
       data(){
           return{
-
+            itemIndex:0,
           }
+      },
+      //计算属性
+      // computed:mapGetters({
+      //   ''
+      // }),
+      created(){
+      },
+      mounted(){
+          console.log(444)
+        if(window.TABBAR_STATUS !== -1){
+          this.itemIndex = window.TABBAR_STATUS;
+          console.log(555)
+       }
+      },
+      // 监听,当路由发生变化的时候执行
+      watch:{
+        $route(to,from){
+          console.log('$route',this.$route);
+          console.log('to',to,'from',to);
+          if((window.TABBAR_STATUS !== -1) && (from.path == '/orderdetail' || from.path == '/productdetail')){
+            this.itemIndex = window.TABBAR_STATUS;
+            console.log('this.itemIndex',this.itemIndex)
+          }
+        }
       },
       methods:{
         onIndexChange(newIndex, oldIndex){
-          console.log(newIndex, oldIndex);
+          console.log('new',newIndex, 'old',oldIndex);
         }
       }
     }
