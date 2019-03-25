2104d9e58e35e6bb75b97ef1c83fe5d88bf4a71f
wayne <1005881270@qq.com>
2019-03-25 19:02:12 +0800

    main状态保持

--- src/main.js
+++ src/main.js
@@ -15,6 +15,7 @@ import {
   TabbarItem,
   Tab,
   TabItem,
+  AlertPlugin ,
 }from 'vux'
 
 Vue.component('x-header', XHeader);
@@ -25,6 +26,7 @@ Vue.component('tabbar', Tabbar);
 Vue.component('tabbar-item', TabbarItem);
 Vue.component('tab', Tab);
 Vue.component('tab-item', TabItem);
+Vue.use(AlertPlugin);
 
 // 将axios挂载到prototype上，在组件中可以直接使用this.axios访问
 Vue.prototype.axios = axios;
@@ -34,6 +36,17 @@ require('./mock.js');
 
 //保存tabbar状态
 window.TABBAR_STATUS = -1;
+
+// //获取open_id
+// let cur_url = window.location.href; //
+// // let cur_url = 'http://ssdsd?open_id=oUgkh1Tg7Zv7UQAmNfH2skiCYMRo#/';
+// let temp = cur_url.split('=')[1];
+// const open_id = temp.split('#')[0];
+
+//引入自定义js函数
+// import * as COMMON from './assets/js/common.js'
+// Vue.prototype.common = COMMON;
+// Vue.use(COMMON);
 
 FastClick.attach(document.body);
 
